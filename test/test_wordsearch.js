5c8a337b4afac8bf2a3bcfd2a8454a571df17f55
Kennie <Kenneth Chan>
2022-11-23 22:57:48 -0500

    word search function without stretch

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present (horizontally)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +32,44 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word search is empty", function () {
+    const result = wordSearch([], 'SEINFELD')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (vertically", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'N', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'E', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'M', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'A', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'I', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CINEMATIC')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if there is no word", function () {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+	if (!letters.length) {
+		return false;
+	}
+	const horizontalJoin = letters.map(ls => ls.join(''))
+	for (l of horizontalJoin) {
+		if (l.includes(word)) return true
+	}
+	let vertical = transpose(letters);
+	const verticalJoin = vertical.map(ls => ls.join(''))
+	for (l of verticalJoin) {
+		if (l.includes(word)) return true
+	}
+	return false;
+};
+
+const transpose = function (matrix) {
+	let finalMatrix = [];
+	for (let i = 0; i < matrix[0].length; i++) {
+		let stagingArr = [];
+		matrix.forEach(e => stagingArr.push(e[i]));
+		finalMatrix.push(stagingArr);
+	}
+	return finalMatrix;
+};
 
 module.exports = wordSearch
